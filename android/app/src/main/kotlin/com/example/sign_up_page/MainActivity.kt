4cf8f7d664b2f38914b8673599af0eea0b4db408
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2022-02-02 08:59:31 +0100

    Create the project

---
+++ android/app/src/main/kotlin/com/example/sign_up_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sign_up_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() =>
+    runApp(MaterialApp(
+      home: SignUpPage(),
+    ));
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+}
+
