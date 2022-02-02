6b67113c19d0ea8634770ca4a62c8875928baf84
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2022-02-02 18:47:20 +0100

    create appBar

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -11,16 +10,37 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: 200,
-          decoration: BoxDecoration(
-            color: Color(0xff4577FA),
-            borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(500),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xff4577FA),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.grey[400]),
+          onPressed: () {},
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: Color(0xff4577FA),
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(1000),
+              ),
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
+            child: Text(
+              'Log In',
+              style: TextStyle(
+                  color: Color(0xff4577FA),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
     );
   }
