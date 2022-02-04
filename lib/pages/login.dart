bc740a600ac2e8d1c40537da098cdd18e7aa7ed7
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2022-02-04 19:46:31 +0100

    Add social media buttons and new user button

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -15,16 +16,16 @@ class _LoginState extends State<Login> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Color(0xff4577FA),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {},
-        ),
+//        leading: IconButton(
+//          icon: Icon(Icons.arrow_back, color: Colors.white),
+//          onPressed: () {},
+//        ),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            height: 150,
+            height: 80,
             decoration: BoxDecoration(
               color: Color(0xff4577FA),
               borderRadius: BorderRadius.only(
@@ -33,110 +34,175 @@ class _LoginState extends State<Login> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-            child: Text(
-              'Log In',
-              style: TextStyle(
-                  color: Color(0xff4577FA),
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                prefixIcon: Icon(
-                  FontAwesomeIcons.envelope,
-                  size: 17.0,
-                  color: Color(0xff4577FA),
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 20.0),
+                Text(
+                  'Log In',
+                  style: TextStyle(
+                      color: Color(0xff4577FA),
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold),
                 ),
-                fillColor: Colors.white,
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Color(0xffDBDBDB), fontSize: 14.0),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(color: Color(0xffCACACA), width: 1.0),
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.envelope,
+                      size: 17.0,
+                      color: Color(0xff4577FA),
+                    ),
+                    fillColor: Colors.white,
+                    hintText: 'Email',
+                    hintStyle:
+                        TextStyle(color: Color(0xffDBDBDB), fontSize: 14.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          BorderSide(color: Color(0xffCACACA), width: 1.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff0066B4), width: 2.0),
+                    ),
+                  ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(color: Color(0xff0066B4), width: 2.0),
+                SizedBox(height: 10.0),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.lock,
+                      size: 17.0,
+                      color: Color(0xff4577FA),
+                    ),
+                    fillColor: Colors.white,
+                    hintText: 'Password',
+                    hintStyle:
+                        TextStyle(color: Color(0xffDBDBDB), fontSize: 14.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          BorderSide(color: Color(0xffCACACA), width: 1.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff0066B4), width: 2.0),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                prefixIcon: Icon(
-                  FontAwesomeIcons.lock,
-                  size: 17.0,
-                  color: Color(0xff4577FA),
-                ),
-                fillColor: Colors.white,
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Color(0xffDBDBDB), fontSize: 14.0),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(color: Color(0xffCACACA), width: 1.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(color: Color(0xff0066B4), width: 2.0),
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Checkbox(
-                  checkColor: Colors.white,
-                  activeColor: Color(0xff4577FA),
-                  value: checkedValue,
-                  onChanged: (bool value) {
-                    setState(
-                      () {
-                        this.checkedValue = value;
+                Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Colors.white,
+                      activeColor: Color(0xff4577FA),
+                      value: checkedValue,
+                      onChanged: (bool value) {
+                        setState(
+                          () {
+                            this.checkedValue = value;
+                          },
+                        );
                       },
-                    );
-                  },
-                  //controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                      //controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                    ),
+                    Text(
+                      'Remember me ?',
+                      style:
+                          TextStyle(fontSize: 13.0, color: Color(0xff4577FA)),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Forgot password ?',
+                      style:
+                          TextStyle(fontSize: 13.0, color: Color(0xffDA4B36)),
+                    ),
+                  ],
                 ),
-              ),
-              Text(
-                'Remember me ?',
-                style: TextStyle(fontSize: 13.0, color: Color(0xff4577FA)),
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(right: 20),
-                child: Text(
-                  'Forgot password ?',
-                  style: TextStyle(fontSize: 13.0, color: Color(0xffDA4B36)),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                  color: Color(0xff4577FA),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: Text(
+                    'Log in with your account',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-            child: RaisedButton(
-              padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              textColor: Colors.white,
-              onPressed: () {},
-              color: Color(0xff4577FA),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
-              ),
-              child: Text(
-                'Log in with your account',
-                style: TextStyle(fontSize: 16.0),
-              ),
+                SizedBox(height: 30.0),
+                Text(
+                  'Log in with',
+                  style: TextStyle(
+                      color: Color(0xff4577FA),
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    RaisedButton.icon(
+                      onPressed: () {},
+                      color: Color(0xff4577FA),
+                      icon: Icon(
+                        FontAwesomeIcons.facebook,
+                        size: 17.0,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        "Facebook",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    RaisedButton.icon(
+                      onPressed: () {},
+                      color: Color(0xffDB4A39),
+                      icon: Icon(
+                        FontAwesomeIcons.google,
+                        size: 17.0,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        "Google",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 100),
+                RaisedButton(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  textColor: Color(0xff4577FA),
+                  onPressed: () {},
+                  elevation: 0,
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    side: BorderSide(color: Color(0xff4577FA), width: 1),
+                  ),
+                  child: Text(
+                    'New user ? Create account',
+                    style: TextStyle(fontSize: 16.0,
+                    fontWeight: FontWeight.w300
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
